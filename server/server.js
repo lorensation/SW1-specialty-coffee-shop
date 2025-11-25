1b6b28c1d35088b1576e761124e343bf6019a731
lorensation <lsanztrucharte@gmail.com>
2025-11-25 15:24:49 +0100

    renamed frontend and backend with client-server

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use(helmet({
 
 // CORS configuration
 app.use(cors({
-  origin: config.frontendUrl,
+  origin: config.cors.origin,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
